5f131cce94b55d5813fa85b32222a180c2d48523
Dhinakardhina <dhinakarsdhinakars5@gmail.com>
2026-03-11 15:56:16 +0530

    Some changes in the index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app= express();
 
 const { userRoute}= require("./routes/user");
@@ -9,4 +11,10 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
-app.listen(3000);
+async function main(){
+    await mongoose.connect("mongodb+srv://2k23csbs09_db_user:1ysz8XZatYtWhmA9@cluster0.3gzyqgl.mongodb.net/coursera-app")
+    app.listen(3000);
+    console.log("Listening on port 3000");
+}
+
+main();
