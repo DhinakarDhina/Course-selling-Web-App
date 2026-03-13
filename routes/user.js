77e56f99afdee3c6370eabb19f99e104b31bec4f
Dhinakardhina <dhinakarsdhinakars5@gmail.com>
2026-03-13 19:35:13 +0530

    Updated the user route

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,18 @@
 const {Router}= require("express");
 const userRoute= Router();
 
-
+// Signup from user
 userRoute.post('/signup', (request, response)=>{
     response.send("Signup Successfully");
 })
 
+
+//Signin from user
 userRoute.post('/signin',(request, response)=>{
     response.send("Sign Successful");
 })
 
+//Purchase course list
 userRoute.get('/purchases',(request, response)=>{
     response.send("Purchased course list");
 })
