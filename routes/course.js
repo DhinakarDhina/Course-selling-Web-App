2d46fa944ac211e3d99c14c34145c144ec3e3fbc
Dhinakardhina <dhinakarsdhinakars5@gmail.com>
2026-03-13 19:31:59 +0530

    Commented on admin route

--- routes/course.js
+++ routes/course.js
@@ -1,10 +1,12 @@
 const Router = require("express");
 const courseRouter= Router();
 
+//Course purchase list
 courseRouter.post('/user/purchases',(request,response)=>{
     response.send("Course purchased successfully");
 })
 
+//course list
 courseRouter.get('/preview', (request, response)=>{
     response.send("Course list");
 })
