0e2716bd5f0830261398c6b79db3852c1647cbe5
Dhinakardhina <dhinakarsdhinakars5@gmail.com>
2026-03-13 19:36:52 +0530

    Updated the index.js file

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ require("dotenv").config();
 
 const app= express();
 
+// Importing the files from the route
+
 const { userRoute}= require("./routes/user");
 const{courseRouter}= require("./routes/course");
 const {adminRouter}= require("./routes/admin");
 
+
+// Routes with version
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
