b56234c644a5e89c3b3e94ca42468fd20aceba25
Dhinakardhina <dhinakarsdhinakars5@gmail.com>
2026-03-15 14:37:00 +0530

    Added the signin and signup feature in admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,63 @@
 const {Router}= require("express");
 const adminRouter = Router();
 const {adminModel}= require("../db");
+const JWT_ADMIN_PASSWORD = "123ldl2345";
+const jwt = require("jsonwebtoken");
+
 
 //bcrypt, zod, jsonwebtoken
 
 // SignUp endpoint
-adminRouter.post("/signup",(request, response)=>{
-    response.send("Signup endpoint");
+adminRouter.post("/signup",async(request, response)=>{
+    const{email, password, firstName, lastName}= request.body;
+    //TODO: hash the password so plaintext pw is not stored in the DB
+    try{
+        await adminModel.create({
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        })
+    } catch(e){
+        response.json({
+            message:"Error"
+        })
+    }
+
+    response.json({
+        message: "Signup Endpoint"
+    })
 })
 
 //Signin Endpoint
-adminRouter.post("/signIn",(request, response)=>{
-    response.send("SignIn endpoint");
+adminRouter.post("/signIn",async(request, response)=>{
+    const{email, password}= request.body;
+
+    //TODO : ideally password should be hashed, and hence  you compare the user provided password and the database password
+
+    const admin = await adminModel.findOne({
+        email: email,
+        password: password
+    });
+    
+    if(admin){
+        const token = jwt.sign({
+            id: admin._id
+        }, JWT_ADMIN_PASSWORD);
+
+        response.json({
+            token : token
+        })
+    }else{
+        response.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
+    
+
+    response.json({
+        message: "SignIn Endpoint"
+    })
 })
 
 //Adding new course
